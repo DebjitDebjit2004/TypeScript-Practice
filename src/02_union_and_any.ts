785b00d36fa4de9dd4c007876030fe36d50b9118
DebjitDebjit2004 <dasdebjit1947@gmail.com>
2026-03-09 16:13:36 +0530

    Lecture-02-complete

---
+++ src/02_union_and_any.ts
@@ -0,0 +1,18 @@
+let apiRequestStatus: "success" | "error" | "pending" = "success";
+
+apiRequestStatus = "error";
+// apiRequestStatus = "Debjit"; // Type 'string' is not assignable to type '"success" | "error" | "pending"'
+
+const orders = ['12','13','14'];
+
+let currentOrder: any;
+
+for (const order of orders) {
+    if (order === '13') {
+        currentOrder = order;
+        break;
+    }
+}
+
+currentOrder = '15';
+console.log(currentOrder);
