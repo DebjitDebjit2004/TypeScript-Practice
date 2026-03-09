0c34da3d73c27d91d2b506741f404120aef97160
DebjitDebjit2004 <dasdebjit1947@gmail.com>
2026-03-09 15:40:27 +0530

    Lecture-01 Complete

---
+++ 01_type_anotation_and_interference.ts
@@ -0,0 +1,8 @@
+let drink = "chai";
+
+let cups = Math.random() > 0.5 ? drink : 0;
+
+let chaiFlavour:string = "masala chai";
+chaiFlavour = "Ginger chai";
+
+// chaiFlavour = 10; // Type 'number' is not assignable to type 'string'
